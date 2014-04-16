6376bfb34591d0dac8d8de6225419834bfd0985c
Alex Nelson <nelsone@nelsone.com>
2014-04-15 20:57:49 -0600

    initial commit.

---
+++ nelsoneCustomProject/src/nelsonecustomproject/NewWizard1.java
@@ -0,0 +1,24 @@
+package nelsonecustomproject;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewWizard1 extends Wizard implements INewWizard {
+
+	public NewWizard1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
